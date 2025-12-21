c11916a0396e505ad9594620de6480396bc95cdb
samuel carter <sam.k.c.0@icloud.com>
2025-12-21 19:30:51 +0000

    use public folder

--- server.js
+++ server.js
@@ -4,12 +4,8 @@ const path = require('path');
 const app = express();
 const port = 9090;
 
-// Serve static files from the current directory
-app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server listening at http://0.0.0.0:${port}`);
